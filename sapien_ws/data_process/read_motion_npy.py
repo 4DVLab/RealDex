5c4303eaf42375a0cb757aa27499d55c647b5906
yyxunn <yyxunn@outlook.com>
2024-01-23 15:27:41 +0800

    drive for real optimation

--- sapien_ws/data_process/read_motion_npy.py
+++ sapien_ws/data_process/read_motion_npy.py
@@ -23,10 +23,12 @@ if __name__ == "__main__":
     trajectory_path = "../config/hand_points.txt"
     trajectory_list = []
     mapping_dir= {}
-   
+    
     for link in sr_struct["hand_info"]:
          mapping_dir[sr_struct["hand_info"][link]["joint"]] = link
-
+    const_added = 1
+    num = 0
+    total_num = len(data)
     for timestamp in data:
         trajectory = []
 
@@ -36,28 +38,34 @@ if __name__ == "__main__":
             
         trajectory.append(points[mapping_dir[ "rh_WRJ2"]])
         trajectory.append(points[mapping_dir[ "rh_WRJ1"]])
+
         trajectory.append(points[mapping_dir[ "rh_FFJ4"]])
         trajectory.append(points[mapping_dir[ "rh_FFJ3"]])
         trajectory.append(points[mapping_dir[ "rh_FFJ2"]])
-        trajectory.append(points[mapping_dir[ "rh_FFJ1"]])
+        trajectory.append(points[mapping_dir[ "rh_FFJ1"]] + const_added*(num/total_num) )
+
         trajectory.append(points[mapping_dir[ "rh_MFJ4"]])
         trajectory.append(points[mapping_dir[ "rh_MFJ3"]])
         trajectory.append(points[mapping_dir[ "rh_MFJ2"]])
-        trajectory.append(points[mapping_dir[ "rh_MFJ1"]])
+        trajectory.append(points[mapping_dir[ "rh_MFJ1"]] + const_added*(num/total_num) )
+
         trajectory.append(points[mapping_dir[ "rh_RFJ4"]])
         trajectory.append(points[mapping_dir[ "rh_RFJ3"]])
         trajectory.append(points[mapping_dir[ "rh_RFJ2"]])
-        trajectory.append(points[mapping_dir[ "rh_RFJ1"]])
+        trajectory.append(points[mapping_dir[ "rh_RFJ1"]] + const_added*(num/total_num) )
+
         trajectory.append(points[mapping_dir[ "rh_LFJ5"]])
         trajectory.append(points[mapping_dir[ "rh_LFJ4"]])
         trajectory.append(points[mapping_dir[ "rh_LFJ3"]])
         trajectory.append(points[mapping_dir[ "rh_LFJ2"]])
-        trajectory.append(points[mapping_dir[ "rh_LFJ1"]])
+        trajectory.append(points[mapping_dir[ "rh_LFJ1"]] + const_added*(num/total_num) )
+
         trajectory.append(points[mapping_dir[ "rh_THJ5"]])
         trajectory.append(points[mapping_dir[ "rh_THJ4"]])
         trajectory.append(points[mapping_dir[ "rh_THJ3"]])
         trajectory.append(points[mapping_dir[ "rh_THJ2"]])
-        trajectory.append(points[mapping_dir[ "rh_THJ1"]])
+        trajectory.append(points[mapping_dir[ "rh_THJ1"]] + const_added*(num/total_num) )
+        num+=1
         trajectory_list.append(trajectory)
         # print(len(trajectory))
     print(len(trajectory_list))
